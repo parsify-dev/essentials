6e5d33f188ceeae0c4c1cad45f4a4b4b64b685bc
Antoni Kepinski <a@kepinski.me>
2020-04-26 16:07:58 +0200

    remove the scientific function (not needed)

--- test.ts
+++ test.ts
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import {units, bin, oct, hex, sci} from './src';
+import {units, bin, oct, hex} from './src';
 
 test('units', t => {
 	t.is(typeof units, 'object');
@@ -16,8 +16,4 @@ test('octal', t => {
 
 test('hexadecimal', t => {
 	t.is(hex(100), '64');
-});
-
-test('scientific', t => {
-	t.is(sci(100), '1e+2');
 });
